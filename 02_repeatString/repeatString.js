b501631fa7b729674309e20240aa03fe41e02e34
Roy Mamon <mamon8521@gmail.com>
2024-12-14 14:41:33 +0200

    finished 2nd exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) { return "ERROR"}
+     let res = "";
+     for(let i = 1 ; i <= times ; i++){
+        res = res + str;
+     }
+     return res;
 };
 
 // Do not edit below this line
