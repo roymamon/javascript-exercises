2e1bd99dcb88e380abc7c8d0a005418fb49cd4fc
Roy Mamon <mamon8521@gmail.com>
2024-12-14 15:29:53 +0200

    finished 5th exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(posInt1, posInt2) {
+        if(Number.isInteger(posInt1)
+            && Number.isInteger(posInt2)
+            && posInt1 >= 0 && posInt2 >= 0) {
+                let min = Math.min(posInt1, posInt2);
+                let max = Math.max(posInt1, posInt2);
+                let res = 0;
+                for (i = min ; i <= max ; i++) {
+                    res += i;
+                }
+                return res;
+            }
+            return "ERROR";
 };
 
 // Do not edit below this line
