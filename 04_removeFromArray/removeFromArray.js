37bea4a5e3cd412d7745bd28370a9cf5c462ce02
Roy Mamon <mamon8521@gmail.com>
2024-12-14 15:16:25 +0200

    finished 4th exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments); // Convert arguments to an array
+    let res = args[0];
+    for (i = 0 ; i <= res.length-1 ; i++){
+        for (j = 1 ; j <= args.length-1 ; j++){
+            if(res[i] === args[j]) {
+                res.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
