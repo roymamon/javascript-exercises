885ee6d9329a090fecf8ea1dbba10c4457cfa0b0
Roy Mamon <mamon8521@gmail.com>
2024-12-14 14:48:07 +0200

    finished 3rd exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+        let res = "";
+        for (i = str.length-1 ; i>=0 ; i--){
+            res += str[i];
+        }
+        return res;
 };
 
 // Do not edit below this line
