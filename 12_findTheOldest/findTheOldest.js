b42fafb96f12cef8ee4b2881e92d8c55e998e480
Roy Mamon <mamon8521@gmail.com>
2024-12-18 17:15:31 +0200

    finished exercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear(); // Get the current year
 
+    return people.reduce((oldest, person) => {
+        const yearOfDeath = person.yearOfDeath || currentYear;
+        const age = yearOfDeath - person.yearOfBirth;
+
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        return age > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
