53d887504a71db29b6601600cc0da678618a17c6
Roy Mamon <mamon8521@gmail.com>
2024-12-18 16:13:49 +0200

    finished exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    const filteredStr = str
+    .split("") // Split into an array of characters
+    .filter((char) => /[a-zA-Z0-9]/.test(char))
+    .join("") // Join it back into a string
+    .toLowerCase();
+   /* for(let i = 0, j = filteredStr.length - 1 ; i<j ; i++,j--) {
+        if (filteredStr[i] !== filteredStr[j]) {return false;}
+    }
+    return true;*/
+    const reversedString = filteredStr.split("").reverse().join(""); // Reverse the string
+    return filteredStr === reversedString;
+
 
 };
 
