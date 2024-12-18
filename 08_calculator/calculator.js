a9db051593e05e9959249206baf48f4ed3b84ced
Roy Mamon <mamon8521@gmail.com>
2024-12-18 15:50:59 +0200

    finished exercise 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+  return a-b;
 	
 };
 
-const subtract = function() {
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => acc + curr, 0);
+
+};
+
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr, 1);
+
+};
+
+const power = function(a,b) {
+return Math.pow(a,b);
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(a) {
+      if(a<0){return undefined}
+      let res = 1;
+      for(let i = 1 ; i<=a ; i++){
+        res *=i;
+      }
+      return res;
 	
 };
 
